2b8e09f3aa4c1d910e306937452af0f27665be7e
zhaohuanyu <zhaohuanyu@aliyun.com>
2020-11-29 22:19:27 +0800

    修改目录可嵌套

--- dist/menus/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var menuBtn = document.querySelector('.menu-btn');
-var menuOpen = false;
-menuBtn.addEventListener('click', function () {
-  menuOpen = !menuOpen;
-  menuBtn.classList.toggle('open');
-});
-//# sourceMappingURL=index.js.map
